f39a25362cf8cc268364b9e48e0191d5574b272a
Ramiro <ram.santos@hotmail.com>
2021-05-02 20:54:43 -0300

    arrancando cliente

--- src/upload-file.py
+++ src/upload-file.py
@@ -1,6 +1,35 @@
+from lib.serverParamsValidation import Params
+import os
+
+
 def main():
-    print("hello world!")
-    print("upload-file")
+    host, port, sPath, verbose, quiet, helpParam = Params.validateParams()
+    if helpParam:
+        return printHelp()
+
+    file = open("prueba.txt", "rb")
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0, os.SEEK_SET)
+
+
+
+
+
+def printHelp():
+    print('usage: start-server [-h] [-v|-q] [-H ADDR] [-p PORT] [-s DIRPATH]')
+    print('')
+    print('<command description>')
+    print('')
+    print('optional arguments:')
+    print('-h, --help       show this help message and exit')
+    print('-v, --verbose    increase output verbosity')
+    print('-q, --quiet      decrease output verbosity')
+    print('-H, --host       server IP address')
+    print('-p, --port       server port')
+    print('-s, --src        source file path')
+    print('-n, --name       file name')
+
 
 if __name__ == "__main__":
     main()
