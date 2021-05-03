d6c84600bb121f2e7aa0d8c9135d5ee4c7e6088d
Christian cucco <cdc-96@hotmail.com>
2021-05-03 00:14:14 -0300

    first server logic

--- src/lib/serverConection/serverConection.py
+++ src/lib/serverConection/serverConection.py
@@ -1,4 +1,17 @@
+
+DATA_CHUNK = 1024
+
+
 class ServerConection:
-    def new_client_connected(c, sPath, verbose, quiet):
-        print(sPath)
+    def new_client_connected(s, sPath, verbose, quiet):
+        data = s.recv(DATA_CHUNK)
+        if data.decode() == 'upload':
+            s.send("OK".encode())
+            print("NEW UPLOAD CLIENT")
+        elif data.decode() == 'download':
+            s.send("OK".encode())
+            print("NEW DOWNLOAD CLIENT")
+        else:
+            s.send("ERROR".encode())
+        s.close()
         return
