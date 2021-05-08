2419eb9781ce8deb5b106266a0dd964d8ffa9db3
Guido Negri <guidonegri>
2021-05-08 00:05:41 -0300

    Refactor download-file

--- src/download-file.py
+++ src/download-file.py
@@ -2,6 +2,7 @@ from lib.params.downloadClientParamsValidation import DownloadClientParams
 from lib.constants import Constants
 from lib.fileTransfer.fileTransfer import FileTransfer
 from lib.logger.logger import Logger
+from lib.clientConnection.clientDownload import ClientDownload
 import socket
 
 
@@ -9,6 +10,7 @@ def main():
     host, port, fName, fDest, verb, quiet, h = DownloadClientParams.validate()
     if h:
         return printHelp()
+
     downloadSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     Logger.logIfVerbose(verb, "Download-client socket successfully created")
     try:
@@ -17,39 +19,18 @@ def main():
         downloadSocket.close()
         Logger.log("Could not connect to server")
         return
-    downloadSocket.send("download".encode())
 
-    # "Server ready to download" validation:
-    data = downloadSocket.recv(Constants.bytesChunk())
-    if data.decode() == 'OK':
-        Logger.logIfVerbose(verb, "Server ready to download")
-        downloadSocket.send(fName.encode())
-    elif data.decode() == 'ERROR':
-        Logger.log("Server could not resolve the request")
-        downloadSocket.close()
-        return
+    Logger.log("Client connected with host: " + str(host) + " and port: "
+               + str(port))
 
-    # Filename validation:
-    data = downloadSocket.recv(Constants.bytesChunk())
-    if data.decode() == 'OK':
-        Logger.logIfVerbose(verb, "Server found the file")
-    elif data.decode() == 'ERROR':
-        Logger.log("The file does not exist on the server")
-        downloadSocket.close()
-        return
+    #### LO NUEVO
+    clientDownload = ClientDownload()
 
-    size = downloadSocket.recv(Constants.bytesChunk()).decode()
-    try:
-        f = open(fDest + fName, "wb")
-    except Exception:
-        Logger.log("Client could not create the file on: " + fDest + fName)
-        downloadSocket.close()
-        return
-    FileTransfer.recieveFile(downloadSocket, f, fName, size,
-                             Constants.bytesChunk(), verb, quiet)
-    Logger.log("File downloaded successfully in: " + fDest + fName)
-    f.close()
+    clientDownload.download(downloadSocket, fName, fDest, verb, quiet)
+    ##### LO NUEVO
+
     downloadSocket.close()
+    Logger.log("Client closed")
     return
 
 
---
+++ src/lib/clientConnection/clientDownload.py
@@ -0,0 +1,48 @@
+from lib.constants import Constants
+from lib.fileTransfer.fileTransfer import FileTransfer
+from lib.logger.logger import Logger
+
+
+class ClientDownload:
+    def download(self, downloadSocket, fName, fDest, verb, quiet):
+        Logger.logIfVerbose(verb, "Sending download code to server")
+        downloadSocket.send(Constants.downloadProtocol().encode())
+
+        # "Server ready to download" validation:
+        data = downloadSocket.recv(Constants.bytesChunk())
+        if data.decode() == 'OK':
+            Logger.logIfVerbose(verb, "Server ready to download")
+            downloadSocket.send(fName.encode())
+            Logger.logIfVerbose(verb, "Sending file name to server")
+        elif data.decode() == 'ERROR':
+            Logger.log("Server could not resolve the request")
+            downloadSocket.close()
+            return
+
+        # Filename validation:
+        data = downloadSocket.recv(Constants.bytesChunk())
+        if data.decode() == 'OK':
+            Logger.logIfVerbose(verb, "Server found the file")
+        elif data.decode() == 'ERROR':
+            Logger.log("The file does not exist on the server")
+            downloadSocket.close()
+            return
+
+        # Size validation:
+        try:
+            size = downloadSocket.recv(Constants.bytesChunk()).decode()
+            int(size)
+        except Exception:
+            Logger.log("Invalid file size " + size)
+            return
+
+        try:
+            file = open(fDest + fName, "wb")
+        except OSError:
+            Logger.log("Client could not create the file on: " + fDest + fName)
+        FileTransfer.recieveFile(downloadSocket, file, fName, size,
+                                     Constants.bytesChunk(), verb, quiet)
+        Logger.log("File downloaded successfully in: " + fDest + fName)
+
+        file.close()
+        return
